13fe2cdf38d879e51297fa41dde0b77bf24c880f
Anna <anna.ruotanen@gmail.com>
2017-12-13 09:27:24 +0100

    Install dependencies for testing, create test for title and change the page title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Title content= "Evaluation tool for teachers"/>
+        <Title content= "Evaluation tool"/>
       </div>
     );
   }
